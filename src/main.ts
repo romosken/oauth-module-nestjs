919df93300c91bd2fb9058c5aaa0debf5e470f2e
Rodrigo Mosken <rodrigo.mosken@outlook.com>
2025-07-18 16:38:53 -0300

    bug: adding validation on RefreshTokenDto

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
-import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { INestApplication, ValidationPipe } from "@nestjs/common";
-import {
-  BaseErrorHandler,
-  HttpErrorHandler,
-} from "./config/error-handling.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   putGlobalPipes(app);
-  // putGlobalFilters(app);
   await app.listen(process.env.PORT || 3000);
 }
 
@@ -22,13 +17,5 @@ function putGlobalPipes(app: INestApplication) {
     }),
   );
 }
-
-// function putGlobalFilters(app: INestApplication) {
-//   const httpAdapter = app.get(HttpAdapterHost);
-//   app.useGlobalFilters(
-//     new BaseErrorHandler(httpAdapter),
-//     new HttpErrorHandler(httpAdapter),
-//   );
-// }
 
 bootstrap();
